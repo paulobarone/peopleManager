beece3e687a3896bb4c1558222e4f4fea8319744
Paulo <pmarquesbaronefilho18@protonmail.com>
2024-12-13 21:08:24 -0300

    add: Student class with validate

--- src/main/java/com/peoplemanager/Main.java
+++ src/main/java/com/peoplemanager/Main.java
@@ -1,15 +1,21 @@
 package com.peoplemanager;
 
-import com.peoplemanager.model.People;
+import com.peoplemanager.model.Student;
 
 public class Main {
   public static void main(String[] args) {
-    try {
-      People people = new People("Alice de Souza", 18, "Masculino");
+    float[] grades = { 8, 7.5f, 9.5f, 10 };
 
-      System.out.println("Nome: " + people.getName());
-      System.out.println("Idade: " + people.getAge());
-      System.out.println("Gênero: " + people.getGender());
+    try {
+      Student student = new Student("Alice de Souza", 18, "Feminino", "Análise e Desenvolvimento de Sistemas", grades);
+
+      System.out.println("Nome: " + student.getName());
+      System.out.println("Idade: " + student.getAge());
+      System.out.println("Gênero: " + student.getGender());
+      System.out.println("Curso: " + student.getCourse());
+      for(int i = 0; i < student.getGrades().length; i++) {
+        System.out.println("Nota " + (i + 1) + ": " + student.getGrades()[i]);
+      }
     } catch(IllegalArgumentException e) {
       System.out.println(e.getMessage());
     }
---
+++ src/main/java/com/peoplemanager/model/Student.java
@@ -0,0 +1,40 @@
+package com.peoplemanager.model;
+
+public class Student extends People {
+  private String course;
+  private float[] grades;
+
+  public Student(String name, int age, String gender, String course, float[] grades) {
+    super(name, age, gender);
+
+    validateCourse(course);
+    validateGrades(grades);
+
+    this.course = course;
+    this.grades = grades;
+  }
+
+  public String getCourse() { return course; }
+  public float[] getGrades() { return grades; }
+
+  public void setCourse(String course) { this.course = course; }
+  public void setGrades(float[] grades) { this.grades = grades; }
+
+  private void validateCourse(String course) {
+    if (course == null || course.isEmpty()) {
+      throw new IllegalArgumentException("O aluno precisa estar em um curso");
+    }
+  }
+
+  private void validateGrades(float[] grades) {
+    if (grades == null) {
+      throw new IllegalArgumentException("Insira um valor válido para as notas");
+    }
+
+    for (float grade : grades) {
+      if (grade < 0 || grade > 10) {
+        throw new IllegalArgumentException("Nota inválida. A nota deve ser maior ou igual a 0 e menor ou igual a 10");
+      }
+    }
+  }
+}
